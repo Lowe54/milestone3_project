337c7a2a3cb63347f9113372f7d6d5de967f57cc
Lowe54 <jameslowe241@gmail.com>
2019-08-19 10:36:37 +0000

    Graphs.js updated with second graph, js file reworked to allow chart resetting

--- static/js/scripts/graphs.js
+++ static/js/scripts/graphs.js
@@ -1,53 +1,53 @@
-d3.json('static/data/collection.json', function (error, data){
+drawall();
+function drawall(){
+  d3.json('static/data/collection.json', function (error, data){
+    if(error){
+      console.error(error);
+    }
+    var ndx = crossfilter(data);
+    var difficulty = ndx.dimension(function(d){ return d.recipie_difficulty;});
+    var mealtype = ndx.dimension(function(d){return d.recipie_mealtype;});
+    var difgroup = difficulty.group();
+    var mealgroup = mealtype.group();
 
-  if(error){
-    console.error(error);
-  }
-console.log(data);
+    barchart = dc.barChart('#difficultychart')
+    .width(500).height(500)
+    .dimension(difficulty)
+    .group(difgroup)
+    .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .xAxisLabel("Difficulty")
+    .yAxis()  
+    .ticks(1);
 
-var ndx = crossfilter(data);
-var parseDate = d3.time.format("%Y/%m/%d").parse;
+    mealbarchart = dc.barChart('#mealtypechart')
+    .width(500).height(500)
+    .dimension(mealtype)
+    .group(mealgroup)
+    .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .xAxisLabel("MealType")
+    .yAxis()
+    .ticks(1);
 
-data.forEach(function(d) {
-  console.log("createdDate:" + d.createdDate);
-  var croppedDate = d.createdDate.substr(0,10);
-  console.log("CroppedDate:" + croppedDate);
-  d.date = d3.time.format("%Y/%m/%d").parse(croppedDate);
+    dc.renderAll();
+  });
+}
+
+resetchart = function(){
+  $("#difficultychart").html("");
+  $("#mealtypechart").html("");
+  drawall();
   
-  console.log("Parsed CroppedDate: " + parseDate(croppedDate));
-});
-console.warn("*****************");
-print_filter(data);
-console.warn("*****************");
-var createdDim = ndx.dimension(function(d) { return d.createdDate; }); 
-var hits = createdDim.group();
-var difficulty = ndx.dimension(function(d){ return d.recipie_difficulty;});
-var difgroup = difficulty.group();
-var minDate = createdDim.bottom(1)[0].date;
-var maxDate = createdDim.top(1)[0].date;
-
-// var hitslineChart  = dc.lineChart("#chart");
-// hitslineChart
-// 	.width(500).height(200)
-// 	.dimension(createdDim)
-// 	.group(hits)
-//   .x(d3.time.scale().domain([minDate,maxDate]));
-
-barchart = dc.barChart('#chart')
-.width(500).height(500)
-.dimension(difficulty)
-.group(difgroup)
-.margins({ top: 10, right: 50, bottom: 30, left: 50 })
-.x(d3.scale.ordinal())
-.xUnits(dc.units.ordinal)
-.xAxisLabel("Difficulty")
-.yAxis()
-.ticks(1);
+};
+  
 
 
 
-dc.renderAll();
-});
+
+
 
 //Print filter
 function print_filter(filter){

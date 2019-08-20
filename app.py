6bfd28a8bf7d5f8dfd9a4d9eb7c05050a50c8a90
Lowe54 <jameslowe241@gmail.com>
2019-08-20 18:16:12 +0000

    Auth fix - 4th attempt

--- app.py
+++ app.py
@@ -9,12 +9,10 @@ from testFramework import *
 
 app = Flask(__name__)
 app.config["MONGO_DB_NAME"] = 'recipie_db'
-env = "@milestone4db-c4m84.mongodb.net/recipie_db?retryWrites=true"
 if __name__ == '__main__':
     app.config["MONGO_URI"] = "mongodb+srv://root:os.getEnv(PASSWORD)@milestone4db-c4m84.mongodb.net/recipie_db?retryWrites=true"
 else:
-    password = 'Will0w2L11'
-    app.config["MONGO_URI"] = "mongodb+srv://root:"+password+env
+    app.config["MONGO_URI"] = "mongodb+srv://root:Will0w2L11@milestone4db-c4m84.mongodb.net/recipie_db?retryWrites=true"
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 mongo = PyMongo(app)
